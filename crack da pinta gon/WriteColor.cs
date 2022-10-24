83f5d96e897b1c09fdf965322a96920ff6686d39
nn <vadim.ahtamov@gmail.com>
2022-10-24 12:27:26 +0300

    what da... wheres da percents will be?

--- crack da pinta gon/WriteColor.cs
+++ crack da pinta gon/WriteColor.cs
@@ -19,20 +19,40 @@ namespace crack_da_pinta_gon
 		/// для подстановки цвета используйте "<c>%</c>"
 		/// </param>
 		/// <param name="colors">цвета, подставляемые в строку</param>
-		public void Write(string value, params ConsoleColor[] colors)
+		public static void Write(string value, params ConsoleColor[] colors)
 		{
 			var Default = Console.ForegroundColor; //получение цвета в консоли перед началом работы метода.
 
 			for (int i = 0; i < colors.Length; i++)
 			{
-				for (int j = 0; j < value.Length; j++)
-				{
 
-				}
 			}
 
 			Console.ForegroundColor = Default; //возвращение цвета на изначальный после окончания работы метода.
 		}
-		private string[] Parsed(string value) { return null; }
+		/// <summary>
+		/// разделение строки на подстроки до указанных позиций знака "<c>%</c>".
+		/// </summary>
+		/// <param name="value">строка в которой имеются знаки "<c>%</c>"</param>
+		/// <returns>возвращает подстроки перед знаками "<c>%</c>".</returns>
+		private static string[] Parsed(string value, int percents)
+		{
+			StringBuilder strb = new StringBuilder(value);
+			int added = 0;
+			int lastI = 0;
+			string[] substrings = new string[percents];
+			for (int i = 0; i < strb.Length; i++)
+			{
+				if (value[i] == '%') substrings[added] = Cut(value, lastI, i);
+			}
+			return substrings;
+		}
+
+		private static string Cut(string value, int start, int end)
+		{
+			string cutted = "";
+			for (int i = start; i < end; i++) cutted.Append(value[i]);
+			return cutted;
+		}
 	}
 }
